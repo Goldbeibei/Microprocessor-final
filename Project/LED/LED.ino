4689d153019dc0e1638e70b00e20d5a76fece809
NIUWCSP <121147600+NIUWCSP@users.noreply.github.com>
2023-06-07 12:41:49 +0800

    更新 LED.ino

--- Project/LED/LED.ino
+++ Project/LED/LED.ino
@@ -1,6 +1,6 @@
-const int LED_r_PIN = ;  // LED燈的接腳
-const int LED_b_PIN = ; // LED燈的接腳
-const int LED_g_PIN = ;  //LED燈的接腳
+const int LED_r_PIN = 13;  // LED燈的接腳
+const int LED_b_PIN = 11; // LED燈的接腳
+const int LED_g_PIN = 12;  //LED燈的接腳
 void setup()
 {
   Serial.begin(115200);
@@ -11,22 +11,33 @@ void setup()
 
 void loop() 
 {
- if(t-t1>=0.5&&t>=28)
- { digitalWrite(LED_r_PIN, HIGH);
+// if(t-t1>=0.5&&t>=28)
+// { digitalWrite(LED_r_PIN, HIGH);
+//   digitalWrite(LED_b_PIN, LOW);
+//   digitalWrite(LED_g_PIN, LOW);
+// }  
+// else if(t-t1>=0.5&&t>=24&&t<=28)
+// { digitalWrite(LED_r_PIN, HIGH);
+//   digitalWrite(LED_b_PIN, LOW);
+//   digitalWrite(LED_g_PIN, HIGH);
+// } 
+// else if(t-t1>=0.5&&t<=24)
+// { digitalWrite(LED_r_PIN, LOW);
+//   digitalWrite(LED_b_PIN, HIGH);
+//   digitalWrite(LED_g_PIN, LOW);
+// } 
+   digitalWrite(LED_r_PIN, HIGH);
    digitalWrite(LED_b_PIN, LOW);
    digitalWrite(LED_g_PIN, LOW);
- }  
- else if(t-t1>=0.5&&t>=24&&t<=28)
- { digitalWrite(LED_r_PIN, HIGH);
+   delay(1000);
+   digitalWrite(LED_r_PIN, HIGH);
    digitalWrite(LED_b_PIN, LOW);
    digitalWrite(LED_g_PIN, HIGH);
- } 
- else if(t-t1>=0.5&&t<=24)
- { digitalWrite(LED_r_PIN, LOW);
+   delay(1000);
+   digitalWrite(LED_r_PIN, LOW);
    digitalWrite(LED_b_PIN, HIGH);
    digitalWrite(LED_g_PIN, LOW);
- } 
-
-  
+   delay(1000);
+   
 
 }
